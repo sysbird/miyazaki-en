c9b7fcc2be074d698b5a9baa715e9ed7c54a7ea4
sysbird <koro@sysbird.jp>
2018-01-08 01:35:38 +0900

    add custom post page

---
+++ page-fruits.php
@@ -0,0 +1,55 @@
+<?php get_header();
+	$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+?>
+
+<div id="content">
+	<div class="container">
+
+<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+		<?php if(1 == $paged ): ?>
+			<?php get_template_part( 'content', 'singular' ); ?>
+			<?php if ( wp_is_mobile() ): ?>
+				<?php
+					$page = get_page_by_path( 'calendar' );
+				?>
+				<p><a href="<?php echo get_the_permalink( $page->ID); ?>">&raquo; <?php echo $page->post_title; ?></a></p>
+			<?php else: ?>
+				<?php echo do_shortcode( '[miyazaki_en_fruits_calendar]' ); ?>
+			<?php endif; ?>
+		<?php endif; ?>
+
+		<?php $posts_per_page = get_option( 'posts_per_page' );
+			$offset = $posts_per_page * ( $paged -1 );
+
+			$args = array(
+				'posts_per_page'	=> $posts_per_page,
+				'offset'			=> $offset,
+				'post_type'		=> 'fruits',
+				'post_status'		=> 'publish'
+			);
+
+			$the_query = new WP_Query($args);
+			if ( $the_query->have_posts() ) :
+		?>
+				<div class="tile">
+
+					<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+						<?php get_template_part( 'content', 'fruits' ); ?>
+					<?php endwhile; ?>
+				</div>
+
+		<?php endif;
+			wp_reset_postdata();
+		?>
+
+	</article>
+
+<?php endwhile; ?>
+
+	</div>
+</div>
+
+<?php get_footer(); ?>
---
+++ single-fruits.php
@@ -0,0 +1,47 @@
+<?php get_header(); ?>
+
+<div id="content">
+	<div class="container">
+
+<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<header class="entry-header">
+			<?php echo miyazaki_en_get_catchcopy(); ?>
+			<h1 class="entry-title"><?php the_title(); ?></h1>
+		</header>
+		<div class="entry-content">
+			<?php the_content(); ?>
+		</div>
+
+		<?php $selected = get_field( 'calendar' );  // 収穫カレンダー ?>
+			<?php if(is_array($selected)): ?>
+
+			<table class="fruits-calendar"><tbody><tr><th class="title"><em>&nbsp;</em></th><th class="data"><span>1月</span><span>2月</span><span>3月</span><span>4月</span><span>5月</span><span>6月</span><span>7月</span><span>8月</span><span>9月</span><span>10月</span><span>11月</span><span>12月</span></th></tr>
+
+			<tr>
+				<td class="title">収穫時期</td>
+				<td class="data">
+				<?php for( $i = 1; $i <= 12; $i++ ){
+					if( in_array( $i, $selected) ) { ?>
+						<span class="best"><?php echo $i; ?></span>
+				<?php	}
+					else{ ?>
+						<span><?php echo $i; ?> </span>
+					<?php	}
+				} ?>
+
+			</td></tr></tbody></table>
+		<?php endif; ?>
+
+	</article>
+
+<?php endwhile; ?>
+
+	<?php $page = get_page_by_path( 'fruit' ); ?>
+	<div class="more"><a href="<?php echo get_permalink( $page->ID ); ?>"><?php echo esc_html( get_post_type_object( 'fruits')->label ); ?>をもっと見る</a></div>
+
+	</div>
+</div>
+
+<?php get_footer(); ?>
